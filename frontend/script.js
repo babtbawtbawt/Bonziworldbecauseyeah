e9901ddca27384f57c85276284d0e271078f5ead
babtbawtbawt <j61065154@gmail.com>
2025-08-04 21:57:10 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1917,7 +1917,7 @@ var _createClass = (function () {
                 { type: "text", text: "Mute me then. That's your fucking problem." },
             ],
             [
-                { type: "text", text: "Hey niggers prepare for a joke." },
+                { type: "text", text: "Hey folks prepare for a joke." },
             ],
             [
                 { type: "text", text: "Time to make fun of homophobic people." },
